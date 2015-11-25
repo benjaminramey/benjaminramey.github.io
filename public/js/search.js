6a22dd8d0e667d5d7c459b955d27fc253d130abb
Ben Ramey <ben.ramey@gmail.com>
2015-11-24 22:17:15 -0600

    Search page

---
+++ public/js/search.js
@@ -0,0 +1,40 @@
+var searchModule = angular.module("search", [], function($interpolateProvider) {
+    $interpolateProvider.startSymbol('[[');
+    $interpolateProvider.endSymbol(']]');
+});
+
+searchModule.controller("SearchController", [
+	"$scope", "$http",
+	function($scope, $http) {
+		$scope.searchData = [];		
+		$scope.query = "";
+		$scope.results = [];
+		
+		$http.get("/search.json").then(
+			function(response) {
+				$scope.searchData = response.data;
+			},
+			function() {
+				$scope.searchData = [];
+			}
+		);
+		
+		$scope.search = function() {
+			var lowerQuery = $scope.query.toLowerCase();
+			
+			$scope.results = [];
+			for(var idx in $scope.searchData) {
+				var item = $scope.searchData[idx];
+				console.log(item);
+				if (item.search_omit != "true"
+					&& item.content
+					&& item.title
+					&& (item.content.toLowerCase().indexOf(lowerQuery) >= 0
+						|| item.title.toLowerCase().indexOf(lowerQuery) >= 0))
+				{
+					$scope.results.push(item);
+				}
+			}
+		};
+	}
+]);

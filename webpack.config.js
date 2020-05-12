ff20922362a2447aae199a4a4e600affc4a13b94
benjaminramey <benramey@fastmail.com>
2020-05-11 20:27:23 -0500

    add webpack and remove .sass-cache

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+
+module.exports = {
+    entry: [
+        './assets/js/index.js'
+    ],
+    watch: true,
+    output: {
+        path: path.resolve(__dirname, 'wwwroot/assets'),
+        filename: 'js/main.js'
+    },
+    module: {
+        rules: [
+            // sass loader
+            {
+                test: /\.scss$/,
+                use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'css/[name].css',
+						}
+					},
+					{
+						loader: 'extract-loader'
+					},
+					{
+						loader: 'css-loader'
+					},
+					{
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                includePaths: ['./node_modules']
+                            }
+                        }
+					}
+                ]
+            },
+            // font file loader
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
